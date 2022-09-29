d0cc6712f97a95636f8aac990285f7841212feb2
Artem Solovyov <solovevartem892@gmail.com>
2022-09-29 18:43:48 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [bgi, setBgi] = useState(['bgi_1', 'bgi_2', 'bgi_3', 'bgi_4', 'bgi_5']);
+  const [color, setColor] = useState([212,212,122]);
+  const shuffle = () => {
+    const newArray = bgi.sort(() => Math.random() - 0.5)
+    setBgi([...newArray])
+    setColor([randomInteger(0, 255), randomInteger(0, 255), randomInteger(0, 255)] )
+  }
+  const randomInteger = (min, max) => {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
+  useEffect(() => {
+    let interval = setInterval(shuffle, 52000)
+    return ()=>clearInterval(interval)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className={`header ${bgi[0]}`}>
+        </header>
+        <div className={`left ${bgi[1]}`}></div>
+        <div className={`main ${bgi[2]}`}>
+          <button onClick={shuffle} className={"button"} style={{backgroundColor: `rgb(${color.join(',')})` }}>Click</button>
+        </div>
+        <div className={`right ${bgi[3]}`}></div>
+        <footer className={`footer ${bgi[4]}`}>
+        </footer>
+      </div>
   );
 }
 
